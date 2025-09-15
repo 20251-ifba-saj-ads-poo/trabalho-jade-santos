6e8a8203b8e83df303bbc50cdee78c74e73a2288
eedajy <jaderibeiro46@gmail.com>
2025-09-15 18:50:23 -0300

    limpando

--- src/main/java/br/edu/ifba/saj/fwads/exception/LoginInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifba.saj.fwads.exception;
-
-public class LoginInvalidoException extends Exception {
-    public LoginInvalidoException(String msg){
-        super(msg);
-    }    
-}
